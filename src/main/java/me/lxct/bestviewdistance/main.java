9bebcffcd18f463d0eee5aa6c9e8fcd4e9c73217
BuildTools <unconfigured@null.spigotmc.org>
2018-10-29 21:23:49 +0100

    Start coding.

--- src/main/java/me/lxct/bestviewdistance/main.java
+++ src/main/java/me/lxct/bestviewdistance/main.java
@@ -1,28 +1,24 @@
 package me.lxct.bestviewdistance;
 
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Player;
-import org.bukkit.event.entity.EntityRegainHealthEvent;
-import org.bukkit.event.entity.EntityRegainHealthEvent.RegainReason;
+import org.bukkit.Bukkit;
 
-public class main extends org.bukkit.plugin.java.JavaPlugin implements org.bukkit.event.Listener
+public class main extends org.bukkit.plugin.java.JavaPlugin
 {
     public main() {}
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(this, this);
+        Bukkit.getLogger().info("[BestViewDistance] -------------------------------------------------");
+        Bukkit.getLogger().info("[BestViewDistance] Best View Distance By LXCT => WARNING :");
+        Bukkit.getLogger().info("[BestViewDistance] Make sure you use this plugin with Paper !");
+        Bukkit.getLogger().info("[BestViewDistance] https://papermc.io/");
+        Bukkit.getLogger().info("[BestViewDistance] -------------------------------------------------");
+        getServer().getScheduler().scheduleSyncRepeatingTask(this, () -> {
+            double oldTPS = Bukkit.getTPS()[0];
+            // TESSST
+
+            // TEEEEST
+        }, 0L, 200L);
     }
 
 
 
-    @org.bukkit.event.EventHandler
-    public void regenHealth(EntityRegainHealthEvent event) {
-        if(event.getEntityType() == EntityType.PLAYER) {
-            Player p = (Player) event.getEntity();
-            if (p.hasPermission("uhc.on")) {
-                if(event.getRegainReason() == RegainReason.SATIATED) {
-                    event.setCancelled(true);
-                }
-            }
-        }
-    }
 }
