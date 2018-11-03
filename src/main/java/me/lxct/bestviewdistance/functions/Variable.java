99a83c4b5b8d68dd73892fc6c78d8fc1f36c755f
BuildTools <unconfigured@null.spigotmc.org>
2018-11-03 11:47:05 +0100

    Fix Metrics

--- src/main/java/me/lxct/bestviewdistance/functions/Variable.java
+++ src/main/java/me/lxct/bestviewdistance/functions/Variable.java
@@ -1,6 +1,6 @@
 package me.lxct.bestviewdistance.functions;
 
-import me.lxct.bestviewdistance.Main;
+import me.lxct.bestviewdistance.BestViewDistance;
 import org.bukkit.Location;
 
 import java.util.ArrayList;
@@ -8,14 +8,14 @@ import java.util.HashMap;
 import java.util.List;
 
 public class Variable{
-    public static int max = Main.plugin.getConfig().getInt("ViewDistance.Max");
-    public static int min = Main.plugin.getConfig().getInt("ViewDistance.Min");
+    public static int max = BestViewDistance.plugin.getConfig().getInt("ViewDistance.Max");
+    public static int min = BestViewDistance.plugin.getConfig().getInt("ViewDistance.Min");
     // public static int min = Main.plugin.getConfig().getInt("ViewDistance.Delay"); not needed.
-    public static int rping = Main.plugin.getConfig().getInt("Performances.PingForReduction");
-    public static int aping = Main.plugin.getConfig().getInt("Performances.PingForAugmentation");
-    public static double tpslimit = Main.plugin.getConfig().getDouble("Performances.TPSLimit");
-    public static double tpschange = Main.plugin.getConfig().getDouble("Performances.TPSChangeIndice");
-    public static double maxindice = Main.plugin.getConfig().getDouble("Performances.MaxReductionIndice");
+    public static int rping = BestViewDistance.plugin.getConfig().getInt("Performances.PingForReduction");
+    public static int aping = BestViewDistance.plugin.getConfig().getInt("Performances.PingForAugmentation");
+    public static double tpslimit = BestViewDistance.plugin.getConfig().getDouble("Performances.TPSLimit");
+    public static double tpschange = BestViewDistance.plugin.getConfig().getDouble("Performances.TPSChangeIndice");
+    public static double maxindice = BestViewDistance.plugin.getConfig().getDouble("Performances.MaxReductionIndice");
     static HashMap<String, Location> playerLocation = new HashMap<>();
     public static HashMap<String, Integer> playerViewDistance = new HashMap<>();
     public static List<String> afkList = new ArrayList<>();
