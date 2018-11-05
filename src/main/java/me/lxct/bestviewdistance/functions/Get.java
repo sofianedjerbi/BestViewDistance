4f459722b3f0a5c42a3e0aaecebdba50943f0dfc
BuildTools <unconfigured@null.spigotmc.org>
2018-11-06 00:17:18 +0100

    Added 1.12.X support

--- src/main/java/me/lxct/bestviewdistance/functions/Get.java
+++ src/main/java/me/lxct/bestviewdistance/functions/Get.java
@@ -3,13 +3,27 @@ package me.lxct.bestviewdistance.functions;
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.events.PacketContainer;
+import com.google.common.base.Objects;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import java.lang.reflect.InvocationTargetException;
 
 public class Get extends org.bukkit.plugin.java.JavaPlugin {
-    private static PacketContainer packet;
+    private static PacketContainer handle;
+
+    protected Get(PacketContainer handle, PacketType type) {
+        // Make sure we're given a valid packet
+        if (handle == null)
+            throw new IllegalArgumentException("Packet handle cannot be NULL.");
+        if (!Objects.equal(handle.getType(), type))
+            throw new IllegalArgumentException(handle.getHandle()
+                    + " is not a packet of type " + type);
+
+        Get.handle = handle;
+    }
+
+
     // CALCULATE NEW REDUCTION INDICE
     public static double getNewReductionIndice(Double TPS) {
         if (TPS > Variable.tpslimit && TPS < 20) { // If tps > tps limit
@@ -24,7 +38,7 @@ public class Get extends org.bukkit.plugin.java.JavaPlugin {
         if (Bukkit.getVersion().contains("1.12")) {
             try {
                 ProtocolLibrary.getProtocolManager().recieveClientPacket(player, PacketContainer.fromPacket(PacketType.Play.Client.SETTINGS));
-                return packet.getIntegers().read(0); // Get View Distance
+                return handle.getIntegers().read(0); // Get View Distance
 
             } catch (IllegalAccessException | InvocationTargetException e) {
                 e.printStackTrace(); // ERRORS
