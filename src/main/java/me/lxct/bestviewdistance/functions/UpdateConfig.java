a276fed6419710216f4465723b0cc59d0eeec533
BuildTools <unconfigured@null.spigotmc.org>
2018-12-16 22:19:19 +0100

    Added usePermissions to update config function

--- src/main/java/me/lxct/bestviewdistance/functions/UpdateConfig.java
+++ src/main/java/me/lxct/bestviewdistance/functions/UpdateConfig.java
@@ -10,43 +10,6 @@ public class UpdateConfig {
     public static void updateConfig() {
         FileConfiguration config = BestViewDistance.plugin.getConfig();
         double ver = config.getDouble("Version");
-        if (ver < 1.1) {
-            Bukkit.getLogger().info("[BestViewDistance] Update config.yml...");
-            config.set("Version", 1.1);
-            config.set("Delay.UnsetTeleportViewDelay", 5);
-            config.set("ViewDistance.OnTeleport", 4);
-            BestViewDistance.plugin.saveConfig();
-            Bukkit.getLogger().info("[BestViewDistance] Updated config.yml!");
-            Bukkit.getLogger().info("[BestViewDistance] Added: \"OnTeleport\" and \"UnsetTeleportViewDelay\"");
-            Bukkit.getLogger().info("[BestViewDistance] More information here: https://www.spigotmc.org/resources/61963/updates");
-        }
-        if (ver < 1.2) {
-            Bukkit.getLogger().info("[BestViewDistance] Update config.yml...");
-            config.set("Version", 1.2);
-            config.set("Misc.UseTeleportView", true);
-            BestViewDistance.plugin.saveConfig();
-            Bukkit.getLogger().info("[BestViewDistance] Updated config.yml!");
-            Bukkit.getLogger().info("[BestViewDistance] Added: \"UseTeleportView\"");
-            Bukkit.getLogger().info("[BestViewDistance] More information here: https://www.spigotmc.org/resources/61963/updates");
-        }
-        if (ver < 1.3) {
-            Bukkit.getLogger().info("[BestViewDistance] Update config.yml...");
-            config.set("Version", 1.3);
-            config.set("Features.UseTasks", true);
-            BestViewDistance.plugin.saveConfig();
-            Bukkit.getLogger().info("[BestViewDistance] Updated config.yml!");
-            Bukkit.getLogger().info("[BestViewDistance] Added: \"UseTasks\"");
-            Bukkit.getLogger().info("[BestViewDistance] More information here: https://www.spigotmc.org/resources/61963/updates");
-        }
-        if (ver < 1.4) {
-            Bukkit.getLogger().info("[BestViewDistance] Update config.yml...");
-            config.set("Version", 1.4);
-            config.set("ViewDistance.MoreThanSettings", 0);
-            BestViewDistance.plugin.saveConfig();
-            Bukkit.getLogger().info("[BestViewDistance] Updated config.yml!");
-            Bukkit.getLogger().info("[BestViewDistance] Added: \"MoreThanSettings\"");
-            Bukkit.getLogger().info("[BestViewDistance] More information here: https://www.spigotmc.org/resources/61963/updates");
-        }
         if (ver < 2.0) {
             File f1 = new File("./plugins/BestViewDistance/config.yml");
             File f2 = new File("./plugins/BestViewDistance/old-config.yml");
@@ -62,6 +25,15 @@ public class UpdateConfig {
                     Bukkit.getLogger().info("[BestViewDistance] Failed to delete old config file. Please delete it by yourself.");
                 }
             }
+        }
+        if (ver < 2.1) {
+            Bukkit.getLogger().info("[BestViewDistance] Update config.yml...");
+            config.set("Version", 2.1);
+            config.set("Features.UsePermissions", false);
+            BestViewDistance.plugin.saveConfig();
+            Bukkit.getLogger().info("[BestViewDistance] Updated config.yml!");
+            Bukkit.getLogger().info("[BestViewDistance] Added: \"UsePermissions\"");
+            Bukkit.getLogger().info("[BestViewDistance] More information here: https://www.spigotmc.org/resources/61963/updates");
         }
     }
 }
