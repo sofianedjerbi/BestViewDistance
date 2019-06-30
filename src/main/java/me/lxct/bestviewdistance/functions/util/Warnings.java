35ad6ee2e6527a042677b17224210d4b8a53683b
SofianeDjerbi <sofiane.djerbi38@gmail.com>
2019-06-30 20:29:34 +0200

    Fixed few things

--- src/main/java/me/lxct/bestviewdistance/functions/util/Warnings.java
+++ src/main/java/me/lxct/bestviewdistance/functions/util/Warnings.java
@@ -9,9 +9,10 @@ import static me.lxct.bestviewdistance.functions.data.Variable.serverVersion;
 public class Warnings {
 
     public static void checkProtocolLib() {
-        if (!serverVersion.contains("1.13") || !serverVersion.contains("1.14")) {
+        if (!serverVersion.contains("1.13") && !serverVersion.contains("1.14")) {
             if (Bukkit.getPluginManager().getPlugin("ProtocolLib") == null) {
-                Bukkit.getConsoleSender().sendMessage(colorize("[BestViewDistance] &4&lPROTOCOLLIB IS REQUIRED TO RUN THIS PLUGIN BELOW 1.13.X!"));
+                Bukkit.getConsoleSender().sendMessage(serverVersion);
+                Bukkit.getConsoleSender().sendMessage(colorize("[BestViewDistance] &4&l PROTOCOLLIB IS REQUIRED TO RUN THIS PLUGIN BELOW 1.13.X!"));
                 Bukkit.getConsoleSender().sendMessage(colorize("[BestViewDistance] &4&lPlease download ProtocolLib."));
                 Bukkit.getConsoleSender().sendMessage(colorize("[BestViewDistance] &4&lLink for ProtocolLib: https://www.spigotmc.org/resources/1997/"));
                 Bukkit.getPluginManager().disablePlugin(BestViewDistance.plugin);
